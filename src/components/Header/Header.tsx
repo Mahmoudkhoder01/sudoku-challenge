82aad1af5af69ec8d59e7576653167f63ea668fa
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2024-11-19 18:54:12 +0200

    finisht the work on the functions to make a better clean code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,24 @@
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store";
+import { generateNewGame } from "../../redux/BoardSlice";
 import "./Header.css";
 
 const Header = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleNewGame = () => {
+    dispatch(generateNewGame(50));
+  };
+
   return (
     <header className="header">
       <h1>
         Su<span className="header_group-one">do</span>
         <span className="header_group-two">ku</span>
       </h1>
-      <h2>New Game</h2>
+      <h2 onClick={handleNewGame} className="header_generate-button">
+        Generate New Game
+      </h2>
     </header>
   );
 };
