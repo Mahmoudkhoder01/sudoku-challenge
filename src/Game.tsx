31935489f0581b585baa9a74b619ac4af58f1134
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2024-11-27 19:44:35 +0200

    add the alert functionality to the functions

--- src/Game.tsx
+++ src/Game.tsx
@@ -6,13 +6,14 @@ import Numbers from "./components/Numbers/Numbers";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./redux/store";
 import { setBoard } from "./redux/BoardSlice";
-import Button from "./components/Button/Button";
+import Button from "./components/UI-Components/Button/Button";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { CiCircleCheck } from "react-icons/ci";
 import { IoExtensionPuzzle } from "react-icons/io5";
 import { handleCheckClick, handleHintClick } from "./Functions/Buttons";
 import ImageUploader from "./components/ImageUploader/ImageUploader";
 import { isValidEntry } from "./Functions/sudoku";
+import Alert from "./components/UI-Components/Alert/Alert";
 
 interface ButtonProps {
   title: string;
@@ -20,11 +21,22 @@ interface ButtonProps {
   onClick: () => void;
 }
 
+export interface AlertStates {
+  type: "success" | "error" | "info";
+  message: string;
+}
+
+interface SelectedCellStates {
+  row: number;
+  col: number;
+}
+
 function Game() {
-  const [selectedCell, setSelectedCell] = useState<{
-    row: number;
-    col: number;
-  } | null>(null);
+  const [selectedCell, setSelectedCell] = useState<SelectedCellStates | null>(
+    null
+  );
+
+  const [alert, setAlert] = useState<AlertStates | null>(null);
 
   const board = useSelector((state: RootState) => state.board.board);
 
@@ -44,6 +56,14 @@ function Game() {
       const { row, col } = selectedCell;
 
       const isValid = isValidEntry(board, lockedCells, row, col, number);
+
+      if (!isValid) {
+        setAlert({
+          type: "error",
+          message: "Invalid number entry. This violates Sudoku rules.",
+        });
+        return;
+      }
 
       if (!lockedCells.has(`${row}-${col}`) && isValid) {
         const updatedBoard = board.map((rowArray, rowIndex) =>
@@ -76,7 +96,7 @@ function Game() {
     {
       title: "Check Board",
       icon: <CiCircleCheck size={24} />,
-      onClick: () => handleCheckClick(board, fullBoard),
+      onClick: () => handleCheckClick(board, fullBoard, setAlert),
     },
     {
       title: "Solve Board",
@@ -85,12 +105,15 @@ function Game() {
     },
   ];
 
+  // Close the alert
+  const closeAlert = () => setAlert(null);
+
   return (
     <div className="container">
       <Header />
 
       <div className="inner-container">
-        <GameTable setSelectedCell={setSelectedCell} />
+        <GameTable setSelectedCell={setSelectedCell} setAlert={setAlert} />
 
         <div className="game-side-section">
           <Difficulty />
@@ -100,10 +123,19 @@ function Game() {
           {buttons.map((button, index) => {
             return <Button key={index} {...button} />;
           })}
-
-          <ImageUploader />
         </div>
       </div>
+
+      {/* Show the Alert component if there's an active alert */}
+      {alert && (
+        <Alert
+          type={alert.type}
+          message={alert.message}
+          onClose={closeAlert} // Close alert when the close button is clicked
+        />
+      )}
+
+      <ImageUploader />
     </div>
   );
 }
